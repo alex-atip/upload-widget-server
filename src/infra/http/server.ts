ee9d8113f95fa13a72c033b78bc9498f4a0ded98
Alexandre Oliveira <alexandre.yukon@gmail.com>
2025-08-10 20:34:10 -0400

    feat: add environment validation with Zod and fix server configuration

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,10 +1,13 @@
 import { fastify } from "fastify";
 import fastifyCors from "@fastify/cors";
+import { env } from "../../env.js";
 
 const server = fastify()
 
 server.register(fastifyCors, { origin: "*" })
 
-server.listen({ port:3333, host: '0.0.0.0' }).then(() => {
-  console.log('HTTP server runing!')
+console.log(env.DATABASE_URL)
+
+server.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
+  console.log('HTTP server running!')
 })
