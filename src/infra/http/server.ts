43ec60b15557564d894688cb35efa378fe7254f9
Alexandre Oliveira <alexandre.yukon@gmail.com>
2025-08-12 09:53:28 -0400

    feat: setup HTTP server with Fastify and upload image route

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,13 +1,34 @@
-import { fastify } from "fastify";
-import fastifyCors from "@fastify/cors";
 import { env } from "../../env.js";
+import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
 
-const server = fastify()
+const server = fastify();
 
-server.register(fastifyCors, { origin: "*" })
+server.setValidatorCompiler(validatorCompiler);
+server.setSerializerCompiler(serializerCompiler);
 
-console.log(env.DATABASE_URL)
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: "VAlidation error",
+      issues: error.validation,
+    });
+  }
 
-server.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
-  console.log('HTTP server running!')
-})
+  console.log(error);
+
+  return reply.status(500).send({ message: "Internal server error." });
+});
+
+server.register(fastifyCors, { origin: "*" });
+
+console.log(env.DATABASE_URL);
+
+server.listen({ port: env.PORT, host: "0.0.0.0" }).then(() => {
+  console.log("HTTP server running!");
+});
